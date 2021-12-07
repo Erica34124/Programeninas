6786e8d1c730099207331bb192571412e2510f02
Érica Rodrigues <71781669+Erica34124@users.noreply.github.com>
2021-12-06 23:00:15 -0300

    adicionando funções
    criando botões no formulário

--- script.js
+++ script.js
@@ -1,82 +1,58 @@
+import BotaoDeleta from "./componentes/deletaTarefa.js";
+import BotaoConclui from "./componentes/concluiTarefa.js";
 //entradas
 
-const nomeCliente = document.getElementById("nome");
-const tel = document.getElementById("telefone");
-const email = document.getElementById("email");
-const come = document.getElementById("comentarios");
-const lista = document.getElementById("lista");
-
-//saidas
-const retornarNome = document.getElementById("retornoNome");
-const retornarTel = document.getElementById("retornoTel");
-const retornarEmail = document.getElementById("retornoEmail");
-const retornarCome = document.getElementById("retornoCome");
-// botao
 const botao = document.querySelector(".btn");
+
 // criando função
 
-const tarefa = (evento)=>{
-    evento.preventDefault();
-    const valorCliente = nomeCliente.value;
-    retornarNome.innerText = valorCliente;
-    retornarTel.innerHTML = tel.value;
-    retornarEmail.innerHTML = email.value;
-    retornarCome.innerHTML = come.value;
+const tarefa = (evento) => {
+    evento.preventDefault()
 
+    const lista = document.getElementById("lista");
+    const nomeCliente = document.getElementById("nome");
+    const cliente = nomeCliente.value;
+    const tel = document.getElementById("telefone");
+    const clienteTel = tel.value;
+    const email = document.getElementById("email");
+    const clienteEmail = email.value;
+    const come = document.getElementById("comentarios");
+    const clienteCome = come.value;
 
-//criando elementos
-    // const criarLista = document.createElement("li");
-    // const ListaValor = `<h1>${retornarNome}</h1>`;
-    // criarLista.innerHTML = ListaValor
-    // lista.appendChild(criarLista)
+    //criando elemento
+    const criarLista = document.createElement("li")
+    criarLista.classList.add("config")
+    const ListaValor = `<h3>${cliente}</h3>`;
+    criarLista.innerHTML = ListaValor;
+    lista.appendChild(criarLista);
+    nomeCliente.value = ' '
 
+    const criarListaTel = document.createElement("li")
+    criarListaTel.classList.add("config")
+    const ListaValorTel = `<p>${clienteTel}</p>`;
+    criarListaTel.innerHTML = ListaValorTel;
+    lista.appendChild(criarListaTel)
+    tel.value = ' '
 
+    const criarListaEmail = document.createElement("li")
+    criarListaEmail.classList.add("config")
+    const ListaValorEmail = `<p>${clienteEmail}</p>`;
+    criarListaEmail.innerHTML = ListaValorEmail;
+    lista.appendChild(criarListaEmail)
+    email.value = ' '
+
+    const criarListaCome = document.createElement("li")
+    criarListaCome.classList.add("config")
+    const ListaValorCome = `<p>${clienteCome}</p>`;
+    criarListaCome.innerHTML = ListaValorCome;
     
+    lista.appendChild(criarListaCome)
+    lista.appendChild(BotaoConclui())
+    lista.appendChild(BotaoDeleta())
+    come.value = ' '
+
 }
-
- botao.addEventListener("click", tarefa)
-
-// const botaoDeleta = ()=>{
-
-// }
-
-
-
-// //evento de click
-// botaoDeleta.addEventListener("click", deleta)
-
-
-
-
-
-
-
-
-
-
-
-
-
-//teste de objeto
-
-function Armazenar(){
-    
-        this.nome= "nome",
-        this.tel= 1,
-        this.email="email",
-        this.comentario= "comentario"
-
-    }
-// const adicionar = addEventListener("click", ()=>{
-// const cliente = nomeCliente.Value;
-// cliente = new Armazenar();
-// cliente.tel = tel;
-// cliente.email = email;
-// cliente.comentario = come.Value;
-
-// console.log(cliente)
-// })
-
-
+// chamando a função no evento
+botao.addEventListener("click", tarefa);
 
 
